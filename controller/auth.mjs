224b6abfe1b60bcd30d5d836dcde894245884674
김현용 <multikhy0612@gmail.com>
2025-11-24 14:13:02 +0900

    V 1.0.1 로그인 수정

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -8,22 +8,21 @@ const secretkey = config.jwt.secertKey;
 const bcrSalt = config.bycrypt.saltRounds;
 const jwtExpire = config.jwt.expiresInSec;
 
-async function createJWT(id) {
-  return jwt.sign({ id }, secretkey, { expiresIn: jwtExpire });
+async function createJWT(idx) {
+  return jwt.sign({ idx }, secretkey, { expiresIn: jwtExpire });
 }
 
 // 회원가입
 export async function signup(req, res, next) {
-  const { userid, password, name, email } = req.body;
+  const { userid, password, name, email, url } = req.body;
   // 회원 중복 체크
-  const found = await AR.findByUserid(userid);
+  const found = await AR.findByuserId(userid);
   if (found) {
     return res.status(409).json({ message: `${userid}이 이미 있습니다.` });
   }
   const hashed = bcr.hashSync(password, bcrSalt);
-  const user = await AR.addUser(userid, hashed, name, email);
-  const token = await createJWT(user.id);
-  console.log(token);
+  const user = await AR.addUser({ userid, password: hashed, name, email, url });
+  const token = await createJWT(user.idx);
   res.status(210).json({ token, user });
 }
 
@@ -43,7 +42,7 @@ export async function signup(req, res, next) {
 
 export async function login(req, res, next) {
   const { userid, password } = req.body;
-  const user = await AR.findByUserid(userid);
+  const user = await AR.findByuserId(userid);
   if (!user) {
     res.status(401).json(`${userid}를 찾을 수 없습니다.`);
   }
@@ -52,14 +51,17 @@ export async function login(req, res, next) {
   if (!isvalid) {
     res.status(401).json({ message: "아이디 또는 비밀번호 확인" });
   }
-  const token = await createJWT(user.id);
+  const token = await createJWT(user.idx);
+  console.log(token);
   res.status(200).json({ token, user });
 }
 
 export async function me(req, res, next) {
-  // const user = await Array.findByUserid(req.id);
-  // if (!user) {
-  //   return res.status(404).json({ message: "일치하는 사용자가 없습니다." });
-  // }
-  // res.status(200).json({ token: req.token, userid: user.userid });
+  const user = await AR.findById(req.idx);
+  if (!user) {
+    return res.status(404).json({ message: "일치하는 사용자가 없습니다." });
+  }
+  res
+    .status(200)
+    .json({ /*token: req.token,*/ idx: user.idx, userid: user.userid });
 }
